b111b33443aefb205069046f9578e2d48fa43226
kolllor33 <starwars2@hotmail.be>
2017-08-18 15:11:04 +0200

    First commit

---
+++ Filehandler.js
@@ -0,0 +1,97 @@
+"use strict";
+
+var fs = require("fs"),
+    crypto = require("crypto")
+
+module.exports = class {
+    constructor(args) {
+        const hash = crypto.createHash('sha512')
+        hash.update(args.key)
+        this.path = args.path
+        this.key = hash.digest('base64')
+    }
+
+    //if callback is null then the file has recently created else the data of the file is past as an object
+    init() {
+        if (this.exist(this.path)) {
+            //when file exist transfer content in to memory
+            var data = fs.readFileSync(this.path, {
+                encoding: "utf8"
+            })
+            return this.decrypt(data)
+        } else {
+            //when file doesn't exist make a file
+            const fd = fs.openSync(this.path, "w")
+            fs.closeSync(fd)
+            fs.writeFileSync(this.path, this.encrypt("{}"))
+            return null
+        }
+    }
+
+    write(data, callback) {
+        fs.writeFile(this.path, this.encrypt(data), "utf8", (err) => {
+            if (err) {
+                onError(err)
+            }
+            callback()
+        })
+    }
+
+    read(callback) {
+        fs.readFile(this.path, 'utf8', (err, data) => {
+            if (err) {
+                onError(err)
+            }
+            callback(this.decrypt(data))
+        })
+    }
+
+    encrypt(data) {
+        var cipher = crypto.createCipher("aes-256-ctr", this.key)
+
+        if (typeof data === 'object') {
+            data = JSON.stringify(data)
+        }
+        var crypted = cipher.update(data, 'utf8', 'base64')
+        crypted += cipher.final('base64')
+
+        return crypted.toString()
+    }
+
+    decrypt(data) {
+        var decipher = crypto.createDecipher("aes-256-ctr", this.key)
+        var dec = decipher.update(data, 'base64', 'utf8')
+        dec += decipher.final('utf8');
+
+        return this.parse(dec)
+    }
+
+    parse(data) {
+        //if isJSON is true return parsed data else return just the data
+        return this.isJSON(data) ? JSON.parse(data) : data
+    }
+
+    /**
+     * @author <https://github.com/chriso/validator.js/blob/master/src/lib/isJSON.js>
+     */
+    isJSON(data) {
+        try {
+            const obj = JSON.parse(data)
+            return !!obj && typeof obj === 'object'
+        } catch (e) { /* ignore */ }
+        return false
+    }
+
+    exist(path) {
+        try {
+            fs.statSync(this.path)
+            return true
+        } catch (err) {
+            return false
+        }
+    }
+}
+
+function onError(err) {
+    throw new Error(err)
+}
---
+++ index.js
@@ -0,0 +1,112 @@
+"use strict";
+
+var _ = require("lodash"),
+    fh = require("./Filehandler")
+
+module.exports = class {
+    constructor(args) {
+        this.db = {}
+        this.path = args.path
+        this.key = args.key
+        this.fileHandler = new fh({
+            path: this.path,
+            key: this.key
+        })
+        var data = this.fileHandler.init()
+        if (data != null) {
+            this.db = data
+        }
+    }
+
+    addTable(name) {
+        if (this.db[name] === undefined) {
+            this.db[name] = []
+            this.update()
+        }
+    }
+
+    getAllTableNames() {
+        return Object.keys(this.db)
+    }
+
+    getTable(name) {
+        if (this.db[name] == undefined) {
+            return
+        }
+        return this.db[name]
+    }
+
+    insert(tablename, data) {
+        if (this.db[tablename] != undefined) {
+            data.id = this.genUUID()
+            this.db[tablename].push(data)
+            this.update()
+        } else {
+            throw new Error("Table wasn't created")
+        }
+    }
+
+    removeAllBy(tablename, args) {
+        if (this.db[tablename] != undefined) {
+            var removed = this.findAll(tablename, args)
+            _.pullAllBy(this.db[tablename], removed)
+            this.update()
+        } else {
+            throw new Error("Table wasn't created")
+        }
+    }
+
+    updateByID(tablename, id, data) {
+        if (this.db[tablename] != undefined) {
+            var updated = _.filter(this.db[tablename], {
+                id: id
+            })
+            if (updated[0] === undefined) {
+                return
+            }
+            const index = _.sortedIndexBy(this.db[tablename], updated[0])
+            Object.assign(this.db[tablename][index], data)
+            this.update()
+        } else {
+            throw new Error("Table wasn't created")
+        }
+    }
+
+    find(tablename, args) {
+        if (this.db[tablename] != undefined) {
+            return _.find(this.getTable(tablename), args)
+        } else {
+            throw new Error("Table wasn't created")
+        }
+    }
+
+    findLast(tablename, args) {
+        if (this.db[name] != undefined) {
+            return _.findLast(this.getTable(tablename), args)
+        } else {
+            throw new Error("Table wasn't created")
+        }
+    }
+
+    findAll(tablename, args) {
+        if (this.db[tablename] != undefined) {
+            return _.filter(this.getTable(tablename), args)
+        } else {
+            throw new Error("Table wasn't created")
+        }
+    }
+
+    genUUID() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+    update() {
+        this.fileHandler.write(this.db, () => {})
+    }
+}
